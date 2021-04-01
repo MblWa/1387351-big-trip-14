68a6b82789e81368c2621ebf6c4ee02b757e8ce7
MblWa <MblWa@users.noreply.github.com>
2021-04-02 01:01:53 +0300

    Fixes

--- src/main.js
+++ src/main.js
@@ -1,42 +1,42 @@
 import {createSiteMenuTemplate} from './view/site-menu.js';
 import {createSiteFilterTemplate} from './view/site-filter.js';
-import {createTripInfoPriceTemplate} from './view/trip-info-price.js';
-import {createTripInfoRouteTemplate} from './view/trip-info-route.js';
-import {createEventsSortTemplate} from './view/events-sort.js';
-import {createEventsListTemplate} from './view/events-list.js';
-import {createNewEventTemplate} from './view/event-create.js';
-import {createEditEventTemplate} from './view/event-edit.js';
-import {createEventTemplate} from './view/event.js';
+import {createInfoPriceTemplate} from './view/info-price.js';
+import {createInfoRouteTemplate} from './view/info-route.js';
+import {createPointsSortTemplate} from './view/points-sort.js';
+import {createPointsListTemplate} from './view/points-list.js';
+import {createPointCreatorTemplate} from './view/point-creator.js';
+import {createPointEditorTemplate} from './view/point-editor.js';
+import {createPointTemplate} from './view/point.js';
 
-const EVENTS_QTY = 3;
+const POINT_COUNT = 3;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteHeaderElement = document.querySelector('.page-header');
-const siteMenuElement = siteHeaderElement.querySelector('.trip-controls__navigation');
-render(siteMenuElement, createSiteMenuTemplate(), 'beforeend');
+const siteHeader = document.querySelector('.page-header');
+const siteMenu = siteHeader.querySelector('.trip-controls__navigation');
+render(siteMenu, createSiteMenuTemplate(), 'beforeend');
 
-const siteFilterElement = siteHeaderElement.querySelector('.trip-controls__filters');
-render(siteFilterElement, createSiteFilterTemplate(), 'beforeend');
+const siteFilter = siteHeader.querySelector('.trip-controls__filters');
+render(siteFilter, createSiteFilterTemplate(), 'beforeend');
 
-const siteTripInfoElement = siteHeaderElement.querySelector('.trip-main');
-render(siteTripInfoElement, createTripInfoRouteTemplate(), 'afterbegin');
+const siteInfo = siteHeader.querySelector('.trip-main');
+render(siteInfo, createInfoRouteTemplate(), 'afterbegin');
 
-const siteTripInfoRouteElement = siteHeaderElement.querySelector('.trip-info');
-render(siteTripInfoRouteElement, createTripInfoPriceTemplate(), 'beforeend');
+const siteInfoRoute = siteHeader.querySelector('.trip-info');
+render(siteInfoRoute, createInfoPriceTemplate(), 'beforeend');
 
-const siteMainElement = document.querySelector('.page-main');
-const siteEventsElement = siteMainElement.querySelector('.trip-events');
-render(siteEventsElement, createEventsSortTemplate(), 'beforeend');
-render(siteEventsElement, createEventsListTemplate(),'beforeend');
+const siteMain = document.querySelector('.page-main');
+const siteEvents = siteMain.querySelector('.trip-events');
+render(siteEvents, createPointsSortTemplate(), 'beforeend');
+render(siteEvents, createPointsListTemplate(),'beforeend');
 
-const siteEventsListElement = siteMainElement.querySelector('.trip-events__list');
-render(siteEventsListElement, createEditEventTemplate(), 'beforeend');
+const siteEventsList = siteMain.querySelector('.trip-events__list');
+render(siteEventsList, createPointCreatorTemplate(), 'beforeend');
 
-for (let i = 0; i < EVENTS_QTY; i += 1) {
-  render(siteEventsListElement, createEventTemplate(), 'beforeend');
+for (let i = 0; i < POINT_COUNT; i++) {
+  render(siteEventsList, createPointTemplate(), 'beforeend');
 }
 
-render(siteEventsListElement, createNewEventTemplate(), 'beforeend');
+render(siteEventsList, createPointEditorTemplate(), 'beforeend');
